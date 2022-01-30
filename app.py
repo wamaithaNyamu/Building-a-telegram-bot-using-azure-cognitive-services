ea78d9128da9cb1ca9ae07aa7dde307e885f2378
Wamaitha <wamaithanyamu@gmail.com>
2022-01-30 18:24:03 +0300

    Ask the user for input on main.py and basic flask api on app.py

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask
+from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import CommandHandler
+from main import GetTextRead, GetTextOcr
+load_dotenv()
+
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def extract_text_from_telegram(update, context):
+    try:
+        print("Uploading to telegram ...")
+    except Exception as ex:
+        update.message.reply_text("Upload an image with text")
+        print(ex)
+
+
+# set up the introductory statement for the bot when the /start command is invoked
+def start(update, context):
+    try:
+        chat_id = update.effective_chat.id
+        context.bot.send_message(chat_id=chat_id,
+                                 text="Hello there, Wamaitha here. Thank you for registering for the event. \n"
+                                      "This bot takes in a photo and applies the azure cognitive service to extract printed or handwritten text from images or pdfs. Have fun while at it! \n"
+                                      "Connect with me  :\n"
+                                      "Linkedin : https://www.linkedin.com/in/wamaithanyamu/\n"
+                                      "Github : https://github.com/wamaithaNyamu \n"
+                                      "Twitter : https://twitter.com/wamaithaNyamu \n")
+    except Exception as ex:
+        print(ex)
+
+
+# run the start function when the user invokes the /start command
+dispatcher.add_handler(CommandHandler("start", start))
+
+dispatcher.add_handler(MessageHandler(Filters.all, extract_text_from_telegram))
+updater.start_polling()
+
+
+# print
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+
+
+def GetTextOcr(file_path):
+    """
+    Takes in a file and does OCR on the document and returns a string
+    of the text extracted during OCR.
+    :param file_path:
+    :return: string
+    """
+    try:
+        print("This function invokes the OCR API on", file_path)
+    except Exception as ex:
+        print(ex)
+
+
+def GetTextRead(file_path):
+    """
+      Takes in a file and extracts handwritten or printed text
+       on the document and returns a string of the text extracted during OCR.
+      :param file_path:
+      :return: string
+    """
+    try:
+       print("This function invokes the READ API on", file_path)
+    except Exception as ex:
+        print(ex)
+
+
+def ask_user_for_input():
+
+    """
+    Asks the user for input from the command line
+
+    """
+    try:
+
+        # Menu for text reading functions
+        print('1: Use OCR API\n2: Use Read API\n3: Read handwriting\nAny other key to quit')
+        command = input('Enter a number:')
+        if command == '1':
+            image_file = os.path.join('images', 'chinua.png')
+            GetTextOcr(image_file)
+        elif command == '2':
+            image_file = os.path.join('images', 'story.pdf')
+            GetTextRead(image_file)
+        elif command == '3':
+            image_file = os.path.join('images', 'wams.png')
+            GetTextRead(image_file)
+
+    except Exception as ex:
+        print(ex)
+
+
+ask_user_for_input()
